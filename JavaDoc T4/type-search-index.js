bed436debe0bff0fb9217178b40bf0e2c1a1c32c
MCJ <150123741+mkts99@users.noreply.github.com>
2024-01-12 08:31:55 +0100

    Add files via upload

---
+++ JavaDoc T4/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"funciones","l":"FuncionEnvioCorreos"},{"p":"funciones","l":"Funciones"},{"p":"<Unnamed>","l":"penelope"},{"p":"funciones","l":"Utilidades"}];updateSearchResults();
